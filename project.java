a60658dbbb6538bb3b8963c32c2a6a75f33b446e
mahamadouc <mahamadou.c@bronxsoftware.org>
2016-05-27 09:19:00 -0400

    Add files via upload

---
+++ project.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class project {
+
+  public static void main(String[] args) {
+Scanner keyboard = new Scanner(System.in);
+
+System.out.println("Hit Enter");
+
+String name = keyboard.nextLine();
+
+if ( name != null ) {
+
+System.out.println("The internet has had an impact on our lives because it lets us communicate and have fun with people all over the world we wouldn't have met otherwise in online video games.");
+
+}
+}
+
+}
